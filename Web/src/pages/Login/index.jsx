c76d7bf3c15462353713efb62e1a8bdbff84f1c8
Jennison Jardim Barbosa Júnior <jennison.junior@codefix.com.br>
2022-10-06 16:52:20 -0300

    Desenvolvido a pagina de login e cadastro padrão

---
+++ Web/src/pages/Login/index.jsx
@@ -0,0 +1,43 @@
+import {Icon, Input} from '~/componentes'
+
+export const Login = () => {
+  return (
+    <div>
+        <header className=" p-4 border-b border-head-300">
+          <div className="container max-w-xl flex justify-center ">
+            <img src="/imgs/logo-fundo-branco.svg" className="w-32 md:w-40" />
+          </div>
+        </header>
+
+        <main className="container max-w-xl p-4">
+          <div className="p-4 flex space-x-4 items-center">
+            <a href="/">
+              <Icon name="arrowLeft" className="h-6"/>
+            </a>
+            <h2 className="text-xl font-bold">Entre na sua conta</h2>
+          </div>
+
+          <form className="p-4 space-y-6">
+            <Input 
+              type="text"
+              name="email"
+              label="Seu e-mail"
+              placeholder="Digite seu e-mail"              
+            />
+            <Input 
+              type="password"
+              name="password"
+              label="Sua senha"
+              placeholder="Digite sua senha"              
+            />
+
+            <button className="w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl">
+              Entrar          
+            </button> 
+
+          </form>
+        </main>
+    </div>
+
+  )    
+}
